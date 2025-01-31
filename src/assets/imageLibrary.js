fd4fddc503c91fc2b3680695f473e1ca11d9f626
zkerkeb <z.kerkeb@gmail.com>
2025-01-31 22:16:16 +0100

    first commit

---
+++ src/assets/imageLibrary.js
@@ -0,0 +1,305 @@
+import bulbasaur from "./pokemons/1.png";
+import ivysaur from "./pokemons/2.png";
+import venusaur from "./pokemons/3.png";
+import charmander from "./pokemons/4.png";
+import charmeleon from "./pokemons/5.png";
+import charizard from "./pokemons/6.png";
+import squirtle from "./pokemons/7.png";
+import wartortle from "./pokemons/8.png";
+import blastoise from "./pokemons/9.png";
+import caterpie from "./pokemons/10.png";
+import metapod from "./pokemons/11.png";
+import butterfree from "./pokemons/12.png";
+import weedle from "./pokemons/13.png";
+import kakuna from "./pokemons/14.png";
+import beedrill from "./pokemons/15.png";
+import pidgey from "./pokemons/16.png";
+import pidgeotto from "./pokemons/17.png";
+import pidgeot from "./pokemons/18.png";
+import rattata from "./pokemons/19.png";
+import raticate from "./pokemons/20.png";
+import spearow from "./pokemons/21.png";
+import fearow from "./pokemons/22.png";
+import ekans from "./pokemons/23.png";
+import arbok from "./pokemons/24.png";
+import pikachu from "./pokemons/25.png";
+import raichu from "./pokemons/26.png";
+import sandshrew from "./pokemons/27.png";
+import sandslash from "./pokemons/28.png";
+import nidoranF from "./pokemons/29.png";
+import nidorina from "./pokemons/30.png";
+import nidoqueen from "./pokemons/31.png";
+import nidoranM from "./pokemons/32.png";
+import nidorino from "./pokemons/33.png";
+import nidoking from "./pokemons/34.png";
+import clefairy from "./pokemons/35.png";
+import clefable from "./pokemons/36.png";
+import vulpix from "./pokemons/37.png";
+import ninetales from "./pokemons/38.png";
+import jigglypuff from "./pokemons/39.png";
+import wigglytuff from "./pokemons/40.png";
+import zubat from "./pokemons/41.png";
+import golbat from "./pokemons/42.png";
+import oddish from "./pokemons/43.png";
+import gloom from "./pokemons/44.png";
+import vileplume from "./pokemons/45.png";
+import paras from "./pokemons/46.png";
+import parasect from "./pokemons/47.png";
+import venonat from "./pokemons/48.png";
+import venomoth from "./pokemons/49.png";
+import diglett from "./pokemons/50.png";
+import dugtrio from "./pokemons/51.png";
+import meowth from "./pokemons/52.png";
+import persian from "./pokemons/53.png";
+import psyduck from "./pokemons/54.png";
+import golduck from "./pokemons/55.png";
+import mankey from "./pokemons/56.png";
+import primeape from "./pokemons/57.png";
+import growlithe from "./pokemons/58.png";
+import arcanine from "./pokemons/59.png";
+import poliwag from "./pokemons/60.png";
+import poliwhirl from "./pokemons/61.png";
+import poliwrath from "./pokemons/62.png";
+import abra from "./pokemons/63.png";
+import kadabra from "./pokemons/64.png";
+import alakazam from "./pokemons/65.png";
+import machop from "./pokemons/66.png";
+import machoke from "./pokemons/67.png";
+import machamp from "./pokemons/68.png";
+import bellsprout from "./pokemons/69.png";
+import weepinbell from "./pokemons/70.png";
+import victreebel from "./pokemons/71.png";
+import tentacool from "./pokemons/72.png";
+import tentacruel from "./pokemons/73.png";
+import geodude from "./pokemons/74.png";
+import graveler from "./pokemons/75.png";
+import golem from "./pokemons/76.png";
+import ponyta from "./pokemons/77.png";
+import rapidash from "./pokemons/78.png";
+import slowpoke from "./pokemons/79.png";
+import slowbro from "./pokemons/80.png";
+import magnemite from "./pokemons/81.png";
+import magneton from "./pokemons/82.png";
+import farfetchd from "./pokemons/83.png";
+import doduo from "./pokemons/84.png";
+import dodrio from "./pokemons/85.png";
+import seel from "./pokemons/86.png";
+import dewgong from "./pokemons/87.png";
+import grimer from "./pokemons/88.png";
+import muk from "./pokemons/89.png";
+import shellder from "./pokemons/90.png";
+import cloyster from "./pokemons/91.png";
+import gastly from "./pokemons/92.png";
+import haunter from "./pokemons/93.png";
+import gengar from "./pokemons/94.png";
+import onix from "./pokemons/95.png";
+import drowzee from "./pokemons/96.png";
+import hypno from "./pokemons/97.png";
+import krabby from "./pokemons/98.png";
+import kingler from "./pokemons/99.png";
+import voltorb from "./pokemons/100.png";
+import electrode from "./pokemons/101.png";
+import exeggcute from "./pokemons/102.png";
+import exeggutor from "./pokemons/103.png";
+import cubone from "./pokemons/104.png";
+import marowak from "./pokemons/105.png";
+import hitmonlee from "./pokemons/106.png";
+import hitmonchan from "./pokemons/107.png";
+import lickitung from "./pokemons/108.png";
+import koffing from "./pokemons/109.png";
+import weezing from "./pokemons/110.png";
+import rhyhorn from "./pokemons/111.png";
+import rhydon from "./pokemons/112.png";
+import chansey from "./pokemons/113.png";
+import tangela from "./pokemons/114.png";
+import kangaskhan from "./pokemons/115.png";
+import horsea from "./pokemons/116.png";
+import seadra from "./pokemons/117.png";
+import goldeen from "./pokemons/118.png";
+import seaking from "./pokemons/119.png";
+import staryu from "./pokemons/120.png";
+import starmie from "./pokemons/121.png";
+import mrMime from "./pokemons/122.png";
+import scyther from "./pokemons/123.png";
+import jynx from "./pokemons/124.png";
+import electabuzz from "./pokemons/125.png";
+import magmar from "./pokemons/126.png";
+import pinsir from "./pokemons/127.png";
+import tauros from "./pokemons/128.png";
+import magikarp from "./pokemons/129.png";
+import gyarados from "./pokemons/130.png";
+import lapras from "./pokemons/131.png";
+import ditto from "./pokemons/132.png";
+import eevee from "./pokemons/133.png";
+import vaporeon from "./pokemons/134.png";
+import jolteon from "./pokemons/135.png";
+import flareon from "./pokemons/136.png";
+import porygon from "./pokemons/137.png";
+import omanyte from "./pokemons/138.png";
+import omastar from "./pokemons/139.png";
+import kabuto from "./pokemons/140.png";
+import kabutops from "./pokemons/141.png";
+import aerodactyl from "./pokemons/142.png";
+import snorlax from "./pokemons/143.png";
+import articuno from "./pokemons/144.png";
+import zapdos from "./pokemons/145.png";
+import moltres from "./pokemons/146.png";
+import dratini from "./pokemons/147.png";
+import dragonair from "./pokemons/148.png";
+import dragonite from "./pokemons/149.png";
+import mewtwo from "./pokemons/150.png";
+import mew from "./pokemons/151.png";
+
+export const pokemonImages = {
+  1: bulbasaur,
+  2: ivysaur,
+  3: venusaur,
+  4: charmander,
+  5: charmeleon,
+  6: charizard,
+  7: squirtle,
+  8: wartortle,
+  9: blastoise,
+  10: caterpie,
+  11: metapod,
+  12: butterfree,
+  13: weedle,
+  14: kakuna,
+  15: beedrill,
+  16: pidgey,
+  17: pidgeotto,
+  18: pidgeot,
+  19: rattata,
+  20: raticate,
+  21: spearow,
+  22: fearow,
+  23: ekans,
+  24: arbok,
+  25: pikachu,
+  26: raichu,
+  27: sandshrew,
+  28: sandslash,
+  29: nidoranF,
+  30: nidorina,
+  31: nidoqueen,
+  32: nidoranM,
+  33: nidorino,
+  34: nidoking,
+  35: clefairy,
+  36: clefable,
+  37: vulpix,
+  38: ninetales,
+  39: jigglypuff,
+  40: wigglytuff,
+  41: zubat,
+  42: golbat,
+  43: oddish,
+  44: gloom,
+  45: vileplume,
+  46: paras,
+  47: parasect,
+  48: venonat,
+  49: venomoth,
+  50: diglett,
+  51: dugtrio,
+  52: meowth,
+  53: persian,
+  54: psyduck,
+  55: golduck,
+  56: mankey,
+  57: primeape,
+  58: growlithe,
+  59: arcanine,
+  60: poliwag,
+  61: poliwhirl,
+  62: poliwrath,
+  63: abra,
+  64: kadabra,
+  65: alakazam,
+  66: machop,
+  67: machoke,
+  68: machamp,
+  69: bellsprout,
+  70: weepinbell,
+  71: victreebel,
+  72: tentacool,
+  73: tentacruel,
+  74: geodude,
+  75: graveler,
+  76: golem,
+  77: ponyta,
+  78: rapidash,
+  79: slowpoke,
+  80: slowbro,
+  81: magnemite,
+  82: magneton,
+  83: farfetchd,
+  84: doduo,
+  85: dodrio,
+  86: seel,
+  87: dewgong,
+  88: grimer,
+  89: muk,
+  90: shellder,
+  91: cloyster,
+  92: gastly,
+  93: haunter,
+  94: gengar,
+  95: onix,
+  96: drowzee,
+  97: hypno,
+  98: krabby,
+  99: kingler,
+  100: voltorb,
+  101: electrode,
+  102: exeggcute,
+  103: exeggutor,
+  104: cubone,
+  105: marowak,
+  106: hitmonlee,
+  107: hitmonchan,
+  108: lickitung,
+  109: koffing,
+  110: weezing,
+  111: rhyhorn,
+  112: rhydon,
+  113: chansey,
+  114: tangela,
+  115: kangaskhan,
+  116: horsea,
+  117: seadra,
+  118: goldeen,
+  119: seaking,
+  120: staryu,
+  121: starmie,
+  122: mrMime,
+  123: scyther,
+  124: jynx,
+  125: electabuzz,
+  126: magmar,
+  127: pinsir,
+  128: tauros,
+  129: magikarp,
+  130: gyarados,
+  131: lapras,
+  132: ditto,
+  133: eevee,
+  134: vaporeon,
+  135: jolteon,
+  136: flareon,
+  137: porygon,
+  138: omanyte,
+  139: omastar,
+  140: kabuto,
+  141: kabutops,
+  142: aerodactyl,
+  143: snorlax,
+  144: articuno,
+  145: zapdos,
+  146: moltres,
+  147: dratini,
+  148: dragonair,
+  149: dragonite,
+  150: mewtwo,
+  151: mew
+}
